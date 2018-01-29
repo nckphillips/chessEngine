11d669d137988d2083dc5e2bfc280242b4dea503
Nicholas <nckphillips@gmail.com>
2018-01-28 18:08:52 -0800

    started work on a function to get the values for every move.

--- play.c
+++ play.c
@@ -1,10 +1,16 @@
 /*This file contains all functions regarding the engine making moves and reporting to
-  the gui.*/
+	the gui.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "play.h"
 #include "bitBoard.h"
 #include "qlearning.h"
+
+/*returns the text string of the best move in the board's current arrangement.*/
+char *get_best_move()
+{
+
+}
 
 void play(Bitboard *b_ptr)
 {
