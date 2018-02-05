9bdd114bbde5ef71408adc59326540679cb2a5ef
Nicholas <nckphillips@gmail.com>
2018-02-05 11:28:00 -0800

    this files will calculate checkmate

---
+++ checkmate.c
@@ -0,0 +1,7 @@
+
+#include "checkmate.h"
+
+int is_checkmate(Bitboard * b_ptr, int color)
+{
+//for all legal moves, make them and see if you are still in check.
+}
---
+++ checkmate.h
@@ -0,0 +1,6 @@
+#ifndef _CHECK_H
+#define _CHECK_H
+
+int is_checkmate(Bitboard * b_ptr, int color);
+
+#endif
