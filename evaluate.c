3cd0a19ba41f0fabab8f685e45ff22fbc5c98af7
Nicholas <nckphillips@gmail.com>
2018-02-17 12:28:55 -0800

    total material function

--- evaluate.c
+++ evaluate.c
@@ -253,6 +253,26 @@ void getFeatures(Bitboard *b_ptr, int features[NUM_FEATURES]){
 } //Gets features for the board
 */
 
+int getTotalMaterial(b_ptr)
+{
+        int value = 0;
+	wValue  = -1 * getValue(b_ptr->wPawns, WPAWN) * count(b_ptr->wPawns); //get the total value of white pawns
+	wValue -= getValue(b_ptr->wKnights, WKNIGHT) * count(b_ptr->wKnights); //get the total value of white knigts
+	wValue -= getValue(b_ptr->wBishops, WBISHOP) * count(b_ptr->wBishops); //get the total value of white bishops
+	wValue -= getValue(b_ptr->wRooks, WROOK) * count(b_ptr->wRooks); //get the total value of white rooks
+	wvalue -= getValue(b_ptr->wQueen, WQUEEN) * count(b_ptr->wQueen); //get the total value of white queen +															  //previous pieces, excluding King
+        wvalue -= getValue(b_ptr->wKing, WKING) * count(b_ptr->wKing);
+
+
+	Value  = getValue(b_ptr->bPawns, BPAWN) * count(b_ptr->bPawns); //get the total value of black pawns
+	Value += getValue(b_ptr->bKnights, BKNIGHT) * count(b_ptr->bKnights); //get the total value of black knigts
+	Value += getValue(b_ptr->bBishops, BBISHOP) * count(b_ptr->bBishops); //get the total value of black bishops
+	Value += getValue(b_ptr->bRooks, BROOK) * count(b_ptr->bRooks); //get the total value of black rooks
+	Value += getValue(b_ptr->bQueen, BQUEEN) * count(b_ptr->bQueen); //get the total value of black queen +
+	value += getValue(b_ptr->bKing, BKING) * count(b_ptr->bKing);
+        return value;
+}
+
 /*finds the value of a move for a given piecetype, based on the position value arrays*/
 int getPositionValue(Bitboard *b_ptr, int piece_type){
 
@@ -472,7 +492,7 @@ switch(piece_type){
 		return value;
 		break;
 }
-return value;
+return value + getTotalMaterial(b_ptr);
 
 }
 
@@ -502,14 +522,14 @@ int evaluate_pawn_structure(Bitboard *b_ptr){
 int same_col_pawns(Bitboard *b_ptr){
 
 	/*Penalizing stacked pawns by (-5)*/
-	
+
 	int value = 0;
 
 	uint64_t	AFILE = 0x0101010101010101;
 
 	uint64_t	BFILE =      squares[b8] | squares[b7] | squares[b6] | squares[b5] |\
 				 squares[b4] | squares[b3] | squares[b2] | squares[b1];
-				 
+
 	uint64_t	CFILE =      squares[c1] | squares[c2] | squares[c3] | squares[c4] |\
 			         squares[c5] | squares[c6] | squares[c7] | squares[c8];
 
@@ -518,20 +538,20 @@ int same_col_pawns(Bitboard *b_ptr){
 
 	uint64_t	EFILE =      squares[e1] | squares[e2] | squares[e3] | squares[e4] |\
 			         squares[e5] | squares[e6] | squares[e7] | squares[e8];
-			         
+
 	uint64_t	FFILE =      squares[f8] | squares[f7] | squares[f6] | squares[f5] |\
 				 squares[f4] | squares[f3] | squares[f2] | squares[f1];
-				 
+
 	uint64_t	GFILE =      squares[g1] | squares[g2] | squares[g3] | squares[g4] |\
 			         squares[g5] | squares[g6] | squares[g7] | squares[g8];
-			         
+
 	uint64_t	HFILE =      squares[h8] | squares[h7] | squares[h6] | squares[h5] |\
 				 squares[h4] | squares[h3] | squares[h2] | squares[h1];
 
 	int temp = count(b_ptr->bPawns & AFILE);
 	if (temp > 1)
 		value += temp;
-		
+
 	temp = count(b_ptr->bPawns & BFILE);
 	if (temp > 1)
 		value += temp;
@@ -539,23 +559,23 @@ int same_col_pawns(Bitboard *b_ptr){
 	temp = count(b_ptr->bPawns & CFILE);
 	if (temp > 1)
 		value += temp;
-		
+
 	temp = count(b_ptr->bPawns & DFILE);
 	if (temp > 1)
 		value += temp;
-		
+
 	temp = count(b_ptr->bPawns & EFILE);
 	if (temp > 1)
 		value += temp;
-		
+
 	temp = count(b_ptr->bPawns & FFILE);
 	if (temp > 1)
 		value += temp;
-		
+
 	temp = count(b_ptr->bPawns & GFILE);
 	if (temp > 1)
 		value += temp;
-		
+
 	temp = count(b_ptr->bPawns & HFILE);
 	if (temp > 1)
 		value += temp;
@@ -651,4 +671,4 @@ int getValue(uint64_t board, unsigned int piece_type){
 		}
 	}
 	return value;
-} 
+}
